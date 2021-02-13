1169d6148511ad8e4c85d422adff481e487ababe
Syndicate555 <saziz028@uottawa.ca>
2021-02-13 11:47:51 -0500

    Update BinarySearchTree.py

--- Algorithms/Python/BinarySearchTree.py
+++ Algorithms/Python/BinarySearchTree.py
@@ -4,20 +4,20 @@ class Node:
         self.leftChild = None
         self.rightChild = None
 
-    def insert(self, data):
-        if self.value == data:
+    def insert(self, data):  # recursive function to insert an element into the tree
+        if self.value == data:  # if the value already exists in the tree, the function returns false
             return False
-        elif self.value > data:
+        elif self.value > data:  # look left of the tree to insert the element since its smaller than the parent node
             if self.leftChild:
                 return self.leftChild.insert(data)
             else:
-                self.leftChild = Node(data)
+                self.leftChild = Node(data)  # if no node exists, create one
                 return True
-        else:
+        else:  # look right of the tree to insert the element since its greater than the parent node
             if self.rightChild:
                 return self.rightChild.insert(data)
             else:
-                self.rightChild = Node(data)
+                self.rightChild = Node(data)  # if no node exists, create one
                 return True
 
 
