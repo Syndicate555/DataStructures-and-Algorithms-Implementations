81ca7d6ec0edd27b6776dcaa33c61a4247f22726
Syndicate555 <saziz028@uottawa.ca>
2021-02-16 01:50:28 -0500

    Update AlgoexpertQuestions.py

--- Algorithms/Python/AlgoexpertQuestions.py
+++ Algorithms/Python/AlgoexpertQuestions.py
@@ -32,3 +32,20 @@ def isValidSubsequence(array, sequence):
             seqIndex += 1
         arrIndex += 1
         return seqIndex == len(sequence)
+
+
+def tournamentWinner(competitions, results):
+    # Write your code here.
+    arr = []
+    home = 0
+    away = 1
+    for i in range(0, len(competitions)):
+        if results[i] == 0:
+            arr.append(competitions[i][away])
+        elif results[i] == 1:
+            arr.append(competitions[i][home])
+    return max(set(arr), key=arr.count)
+
+
+tournamentWinner([["HTML", "C#"], ["C#", "Python"],
+                  ["Python", "HTML"]], [0, 0, 1])
