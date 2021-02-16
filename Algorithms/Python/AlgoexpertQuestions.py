8ee217cf4f9f536a570d990fa5fa2e44aafb1fb3
Syndicate555 <saziz028@uottawa.ca>
2021-02-15 22:59:54 -0500

    Update AlgoexpertQuestions.py

--- Algorithms/Python/AlgoexpertQuestions.py
+++ Algorithms/Python/AlgoexpertQuestions.py
@@ -1,11 +1,10 @@
 def twoNumberSum(array, targetSum):
     # Write your code here.
     # O(n2) time complexity
-	arr = []
-	for i in range(0, len(array)):
-		for j in range(i+1, len(array)):
-			if (array[i] + array[j] == targetSum):
-				arr.append(array[i])
-				arr.append(array[j])
-	return arr
-    pass
+    arr = []
+    for i in range(0, len(array)):
+        for j in range(i+1, len(array)):
+            if (array[i] + array[j] == targetSum):
+                arr.append(array[i])
+                arr.append(array[j])
+    return arr
