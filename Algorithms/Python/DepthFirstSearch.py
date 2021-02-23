e2cc5ed484830dc5de55c0da1660d29d1f73e9c0
Syndicate555 <saziz028@uottawa.ca>
2021-02-23 03:39:52 -0500

    Update DepthFirstSearch.py

--- Algorithms/Python/DepthFirstSearch.py
+++ Algorithms/Python/DepthFirstSearch.py
@@ -21,3 +21,24 @@ def dfs(visited, graph, node):
 
 # Driver Code
 dfs(visited, graph, 'B')
+
+#########################################################
+# Alternate Solution
+########################################################
+
+
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        # Write your code here.
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
