69eb651fee0569d57b4dcc2697b92bc3d33431cc
Syndicate555 <saziz028@uottawa.ca>
2021-02-26 16:47:05 -0500

    Update test.py

--- Algorithms/Python/test.py
+++ Algorithms/Python/test.py
@@ -1,10 +1,10 @@
 def isPalindrome(string):
     # Write your code here.
     s = ''
-    for i in range(len(string)-1, 0, -1):
+    for i in range(len(string)-1, -1, -1):
         s = s + string[i]
-    s = s + string[0]
-    print(s)
+
+    return s == string
 
 
-isPalindrome('abcdcba')
+print(isPalindrome('abcdcba'))
